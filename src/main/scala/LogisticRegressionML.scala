ce85498e41f3c2e97a194e6f9afd5e55704148b3
greg_huang <greg_huang@trend.com.tw>
2016-04-22 19:22:13 +0800

    Add codes from Spark example as reference

--- src/main/scala/LogisticRegressionML.scala
+++ src/main/scala/LogisticRegressionML.scala
@@ -1,11 +1,12 @@
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.sql.functions.max
 import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * Created by greghuang on 4/21/16.
-  */
+ * Created by greghuang on 4/21/16.
+ */
 object LogisticRegressionML extends App {
   val sparkConf = new SparkConf(false)
     .setMaster("local[*]")
@@ -16,16 +17,16 @@ object LogisticRegressionML extends App {
   val sc = new SparkContext(sparkConf)
   val sqlCtx = new SQLContext(sc)
 
-  val data = sqlCtx.read.format("libsvm").load("data/output/training_all_libsvm.txt")
+  val data = sqlCtx.read.format("libsvm").load("data/sample_libsvm_data.txt")
 
-  val rdd = data.map(t => t(0) match {
-    case 1 => Row(1.0, t(1))
-    case _ => Row(0.0, t(1))
-  })
+  //  val rdd = data.map(t => t(0) match {
+  //    case 1 => Row(1.0, t(1))
+  //    case _ => Row(0.0, t(1))
+  //  })
 
-  val binaryData = sqlCtx.createDataFrame(rdd, data.schema)
+  //  val binaryData = sqlCtx.createDataFrame(rdd, data.schema)
 
-  val Array(trainingData, testingData) = binaryData.randomSplit(Array(0.6, 0.4));
+  val Array(trainingData, testingData) = data.randomSplit(Array(0.6, 0.4));
 
   trainingData.cache()
 
@@ -36,9 +37,11 @@ object LogisticRegressionML extends App {
 
   val lrModel = lr.fit(trainingData)
 
-//  println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+  println(s"Coefficients: ${lrModel.coefficients}")
+  println(s"Intercept: ${lrModel.intercept}")
 
   val trainingSummary = lrModel.summary
+
   val objectiveHistory = trainingSummary.objectiveHistory
   objectiveHistory.foreach(loss => println("loss:" + loss))
   val binarySummary = trainingSummary.asInstanceOf[BinaryLogisticRegressionSummary]
@@ -46,11 +49,29 @@ object LogisticRegressionML extends App {
   roc.show()
   println(binarySummary.areaUnderROC)
 
-//  val fMeasure = binarySummary.fMeasureByThreshold
-//  val maxFMeasure = fMeasure.select(max("F-Measure")).head().getDouble(0)
-//  val bestThreshold = fMeasure.where($"F-Measure" === maxFMeasure)
-//    .select("threshold").head().getDouble(0)
-//  lrModel.setThreshold(bestThreshold)
+  val fMeasure = binarySummary.fMeasureByThreshold
+  val maxFMeasure = fMeasure.select(max("F-Measure")).head().getDouble(0)
+  val bestThreshold = fMeasure.where(fMeasure.col("F-Measure").equalTo(maxFMeasure))
+    .select("threshold").head().getDouble(0)
+  lrModel.setThreshold(bestThreshold)
+
+  println("Best:" + bestThreshold)
+
+  testingData.cache()
+
+  val predictions = lrModel.transform(testingData)
+
+  predictions.select("prediction", "label", "probability").show(5)
+
+  val evaluator = new BinaryClassificationEvaluator()
+    .setLabelCol("label")
+    .setRawPredictionCol("rawPrediction")
+    .setMetricName("areaUnderROC")
+
+  val accuracy = evaluator.evaluate(predictions)
+
+  println("ROC:" + accuracy)
+  //println("Test error:" + (1.0 - accuracy))
 
   sc.stop()
 }
